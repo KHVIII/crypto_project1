cd671f2b1263e043ef730cb3988fb9e2bbae8e4e
frosty1313 <10932308+frosty1313@users.noreply.github.com>
2022-10-10 10:04:56 -0400

    Added statistical attempt #1

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import random
+import time
+import freqAnalysis
+import copy
 
 alphabet = ' abcdefghijklmnopqrstuvwxyz'
-max_key_length = 25
-'''Helper function'''
-def get_dictionaries():
-    d_one = open('dictionary_1.txt', 'r').readlines()
-    d_two = open('dictionary_2.txt', 'r').readlines()
+max_key_length = 26
+
+'''Load the two dictionaries and strip end of line characters'''
+def get_dictionaries(d_one = 'dictionary_1.txt', d_two='dictionary_2.txt'):
+    d_one = open(d_one, 'r').readlines()
+    d_two = open(d_two, 'r').readlines()
     
     for index,text in enumerate(d_one):
         d_one[index] = text.strip('\n\r')
@@ -14,21 +18,17 @@ def get_dictionaries():
         d_two[index] = text.strip('\n\r')
     
     return d_one, d_two
-'''
-def find_character_indexes(text):
-    indexes = {}
-    for char in alphabet:
-       indexes[char] = [idx for idx,c in enumerate(text) if char == c]
-    return indexes
-'''
+
 '''Calculate the shifts between two equal length strings'''
 def calculate_shifts(string_one, string_two):
     all_differences = []
-    #print(len(string_one))
-    #print(len(string_two))
-    for i in range(len(string_one)):
-        difference = calc_shift(string_one[i], string_two[i])
-        all_differences.append(difference)
+    try:
+        for i in range(len(string_one)):
+            difference = calc_shift(string_one[i], string_two[i])
+            all_differences.append(difference)
+    except:
+        print('[!] String length difference in calculate_shifts')
+
     return all_differences
 
 '''Calculate the amount x must be shifted by to reach y'''
@@ -38,6 +38,7 @@ def calc_shift(x,y):
         diff = 27+diff
     return diff
     
+'''Shift a character by shift places'''
 def do_shift(char, shift):
     new_index = alphabet.index(char) + shift
     if new_index >=27:
@@ -48,8 +49,7 @@ def do_shift(char, shift):
 def get_chars_at_indexes(indexes, text):
     return [text[i] for i in indexes]
 
-'''Get every i_th character e.g. Get every 5th character
-'''
+'''Get every i_th character e.g. Get every 5th character'''
 def get_all_i_th_chars(text, i, start=0):
     ret = ''
     x = start
@@ -58,25 +58,14 @@ def get_all_i_th_chars(text, i, start=0):
         x+=i 
     return ret
 
-'''
-def find_same_shifts(plain_text, cipher_text):
-    #cipher_indexes = find_character_indexes(cipher_text)
-    pt_indexes = find_character_indexes(plain_text)
-    same_shifts = []
-    for char, indexes in pt_indexes.items():
-        for i in range(len(indexes)):
-            for j in range(i+1, len(indexes)):
-                if cipher_text[indexes[i]] == cipher_text[indexes[j]]:
-                    same_shifts.append((indexes[j]-indexes[i])%27)
-    print(same_shifts)
-'''
-
+'''Build the key based off the known plain text'''
 def build_key(cipher_text, plain_text, length):
     key = []
     for i in range(length):
         key.append(calc_shift(plain_text[i], cipher_text[i]))
     return key
 
+'''Decrypt a text with the key'''
 def decrypt(cipher_text, key):
     ret = ''
     i = 0
@@ -87,6 +76,7 @@ def decrypt(cipher_text, key):
             i = 0
     return ret
 
+'''Check all possible key lengths for consistent shifts in the cipher text'''
 def brute_key_len(cipher_text, plain_text):
     possible_lengths = []
     for key_len in range(1, max_key_length):
@@ -94,28 +84,123 @@ def brute_key_len(cipher_text, plain_text):
         plain_chars = get_all_i_th_chars(plain_text, key_len)
         
         shifts = calculate_shifts(plain_chars, cipher_chars)
-        #print(shifts)
-        if len(list(set(shifts))) == 1:
-            #print(f'Possible key length {shifts[0]}')
-            possible_lengths.append(key_len)
+        possible_lengths.extend(key_len)
     
     return possible_lengths
 
-def find_pt(cipher_text, dictionary):
+'''Solves test_one without bad data added'''
+def test_one(cipher_text, dictionary):
     for d in dictionary:
         possible_key_lengths = brute_key_len(cipher_text, d)
         if len(possible_key_lengths) > 0:
             for key_len in possible_key_lengths:
                 key = build_key(cipher_text, d, key_len)
                 if decrypt(cipher_text, key) == d:
-                    print(f'My guess for the plaintext is: {d}')
-                    return
+                    return d
+    return False
+'''
+def brute_force_garbage(cipher_text, dictionary):
+    for i in len(cipher_text):
+        cipher = f'{cipher_text[:i]}{cipher_text[i+1:]}'
+        ans = test_one(cipher_text, dictionary)
+'''
+def find_repeat_sequences(cipher_text, length = 3):
+    ret = []
+    for i in range(len(cipher_text) - length):
+        sequence = cipher_text[i:i+length]
+        for j in range(i+1, len(cipher_text)-length):
+            if cipher_text[j:j+length] == sequence:
+                ret.append(sequence)
     
+    return list(set(ret))
+
+def find_gaps_between_repeats(cipher_text, repeated_sequences, length=3):
+    gaps = []
+    for sequence in repeated_sequences:
+        mark = None
+        for i in range(len(cipher_text)-length):
+            if cipher_text[i:i+length] == sequence:
+                if mark:
+                    gaps.append(i-mark)
+                mark = i
+    return gaps
     
+def analyze_gaps(gaps):
+    mod_zero = {}
+    for i in range(1, max_key_length+1):
+        mod_zero[i] = 0
+        for gap in gaps:
+            if gap % i == 0:
+                mod_zero[i] += 1
+    
+    #Sort potential key lengths based on most number of mod 0 hits
+    length_candidates = [key for key,val in sorted(mod_zero.items(), key = lambda item: item[1], reverse=True)]
+    return length_candidates
+    
+def candidate_freq_analysis(key_len, cipher_text):
+    groups = []
+    key = []
+    for i in range(key_len):
+        new_group = get_all_i_th_chars(cipher_text, key_len, i)
+        groups.append(new_group)
+        
+    test_key = []
+    for group in groups:
+        scores = []
+        for subkey in range(0,28):
+            pt_candidate = decrypt(group, [subkey])
+            score = freqAnalysis.englishFreqMatchScore(pt_candidate.upper())
+            scores.append((score, subkey))
+        scores = sorted(scores, key = lambda item: item[0], reverse=True)
+        best = scores[0][0]
+        subkeys = [score[1] for score in scores if score[0] == best]
+        test_key.append(subkeys)
+
+    all_keys = [[k] for k in test_key[0]]
+
+    for i in test_key[1:]:
+        placeholder = []
+        for subkey in i:
+            for ak in all_keys:
+                tmp = copy.deepcopy(ak)
+                tmp.append(subkey)
+                placeholder.append(tmp)
+        all_keys = placeholder
+    
+    best_score = 0
+    pt = ''
+
+    for key in all_keys:
+        pt_candidate = decrypt(cipher_text, key)
+        score = freqAnalysis.englishFreqMatchScore(pt_candidate.upper())
+        if score > best_score:
+            best_score = score
+            pt = pt_candidate
+   
+    return (best_score, pt)
+    
+                
+            
+def test_two(cipher_text):
+    repeated_sequences = find_repeat_sequences(cipher_text)
+    gaps = find_gaps_between_repeats(cipher_text, repeated_sequences)
+    key_length_candidates = analyze_gaps(gaps)
+    
+    all_scores = []
+    for key_len_candidate in key_length_candidates:
+        all_scores.append(candidate_freq_analysis(key_len_candidate, cipher_text))
+    
+    best = sorted(all_scores, key = lambda item: item[0], reverse = True)[0]
+    print(best)
+
+def find_pt(cipher_text, dictionary):
+    if d := test_one(cipher_text, dictionary):
+        print(f'My guess for the plaintext is: {d}')
+        return
+    else:
+        score, pt = test_two(cipher_text)
+        print(f'My guess for the plaintext is: {d}')
     
 dictionary_one, dictionary_two = get_dictionaries()
-
 cipher_text = input('Input your ciphertext: ')
-#print(brute_key_len(cipher_text.strip(), dictionary_one[0]))
-#brute_key_len(dictionary_one[1], dictionary_one[0])
 find_pt(cipher_text, dictionary_one)
